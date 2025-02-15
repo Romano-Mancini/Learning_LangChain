468f6015e3f8762644fc95804d253d31be891941
Romano-Mancini <romanomancini03@gmail.com>
2025-02-15 15:32:43 +0100

    Added vector store creation

---
+++ 4_RAG/4.1_vector_store_creation.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+load_dotenv()
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+article_dir = os.path.join(curr_dir, "wikipedia_articles", "formula1.txt")
+db_dir = os.path.join(curr_dir, "db", "chroma_db")
+
+print(f"""Current directory: {curr_dir}
+Article directory: {article_dir}
+Vector Store directory: {db_dir}""")
+
+if not os.path.exists(db_dir):
+    
+    if not os.path.exists(article_dir):
+        raise FileNotFoundError(f"The path {article_dir} does not exist.")
+
+    loader = TextLoader(article_dir)
+    document = loader.load()
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    splitted_documents = text_splitter.split_documents(document)
+
+    print(f"Number of splitted documents: {len(splitted_documents)}.")
+
+    embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    
+    db = Chroma.from_documents(
+        documents=splitted_documents,
+        embedding=embedding_model,
+        persist_directory=db_dir
+    )
+
+    print(len(db.get()['documents']))
